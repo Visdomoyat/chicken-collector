43e5203f96fc22a5660fbd38684063725438aa92
User Name <user@email.com>
2025-05-13 22:45:56 -0400

    Feat: base, path, views, and css

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Chicken
 
 def home(request):
     return HttpResponse('<h1>Hello world</h1>')
 
 def about(request):
     return render(request, 'main_app/about.html')
+
+def chickens_index(request):
+    chickens = Chicken.objects.all()
+    return render(request, 'chickens/index.html', {'chickens' : 'chickens'})
