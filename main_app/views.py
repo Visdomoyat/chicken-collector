8275daeb40eeb86b677a7d3c83856f99744132e6
User Name <user@email.com>
2025-05-14 00:05:26 -0400

    feat: chicken class based view

--- main_app/views.py
+++ main_app/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Chicken
 
+
 def home(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'main_app/about.html')
 
+
+class Chicken:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+chickens = [
+    Chicken('Nugget', 'Plymouth Rock', 'Nugget is a friendly chicken', 2),
+    Chicken('Fluffy', 'Plymouth Rock', 'Fluffy is a fluffy chicken', 1),
+    Chicken('Bella', 'Plymouth Rock', 'Bella is a beautiful chicken', 3),
+]
+
 def chickens_index(request):
-    chickens = Chicken.objects.all()
-    return render(request, 'chickens/index.html', {'chickens' : 'chickens'})
+    return render(request, 'chickens/index.html', {
+        'chickens': chickens
+    })
